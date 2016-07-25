e57f8e22e397cbcec326f7dcfc6a9f489012cba3
Andrew Wong <andrew.wongsf@gmail.com>
2016-07-25 09:31:23 -0400

    wrapped all js into single global variable and added name input
    combined game_ui.js and game.js into one object and

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-!function(){
+var gameModule = (function(Game){
 
 	$(document).ready(function () {
 		$("#board").hide();
@@ -14,234 +14,22 @@
 		var startHTML = "<div class='screen screen-start' id='start'>";
 		startHTML += "<header>";
 		startHTML += "<h1>Tic Tac Toe</h1>";
+		startHTML += "<input type='text' placeholder='Please enter your name' name='username'><br>";
 		startHTML += "<a href='#' class='button'>Start game</a>";
 		startHTML += "</header>";
 		startHTML += "</div>";
 		$("body").append(startHTML);
+		$("#start > header > input[type='text']").css({
+			"margin": "50px 0",
+			"text-align": "center",
+			"width": "250px"
+		});
 		$("a.button").click(function () {
-			GameUI.newGame();
-			GameUI.beginTurn();
+			Game.newGame();
+			Game.beginTurn();
 		});
 	});
 
-	var Game = {
-		
-		winner: "",
+	return Game;
 
-		checkForWin: function () {
-			var boxes = [];
-			boxes = $(".box").toArray();
-			var check = [];
-
-			for (i = 0; i < boxes.length; i++) {
-				if (boxes[i].className.includes("box-filled-1")) {
-					check.push("player1");
-				} else if (boxes[i].className.includes("box-filled-2")) {
-					check.push("player2");
-				} else {
-					check.push("empty");
-				}
-			}
-			
-			if (check[0] !== "empty" && check[0] === check[1] && check[1] === check[2]) {
-
-				this.winner = check[0];
-				this.displayWinner();
-
-			} else if (check[3] !== "empty" && check[3] === check[4] && check[4] === check[5]) {
-
-				this.winner = check[3];
-				this.displayWinner();
-
-			} else if (check[6] !== "empty" && check[6] === check[7] && check[7] === check[8]) {
-				
-				this.winner = check[6];
-				this.displayWinner();
-
-			} else if (check[0] !== "empty" && check[0] === check[3] && check[3] === check[6]) {
-				
-				this.winner = check[0];
-				this.displayWinner();
-
-			} else if (check[1] !== "empty" && check[1] === check[4] && check[4] === check[7]) {
-						
-				this.winner = check[1];
-				this.displayWinner();
-
-			} else if (check[2] !== "empty" && check[2] === check[5] && check[5] === check[8]) {
-						
-				this.winner = check[2];
-				this.displayWinner();
-
-			} else if (check[0] !== "empty" && check[0] === check[4] && check[4] === check[8]) {
-						
-				this.winner = check[0];
-				this.displayWinner();
-
-			} else if (check[2] !== "empty" && check[2] === check[4] && check[4] === check[6]) {
-						
-				this.winner = check[2];
-				this.displayWinner();
-
-			} else if (check.includes("empty") === false){
-						
-				this.winner = "Tie Game";
-				this.displayWinner();
-			}
-		},
-
-		displayWinner: function () {
-			if (this.winner === "player1") {
-
-				$("#finish").removeClass("screen-win-two");
-				$("#finish").removeClass("screen-win-tie");
-				$(".message").html("Winner");
-				$("#finish").addClass("screen-win-one");
-				$("#finish").show();
-				$("#board").hide();
-
-			} else if (this.winner === "player2") {
-
-				$("#finish").removeClass("screen-win-one");
-				$("#finish").removeClass("screen-win-tie");
-				$(".message").html("Winner");
-				$("#finish").addClass("screen-win-two");
-				$("#finish").show();
-				$("#board").hide();
-
-			} else if (this.winner === "Tie Game") {
-
-				$("#finish").removeClass("screen-win-one");
-				$("#finish").removeClass("screen-win-two");
-				$(".message").html("It's a Tie!");
-				$("#finish").addClass("screen-win-tie");
-				$("#finish").show();
-				$("#board").hide();
-
-			}
-		}
-	};
-
-	var GameUI = {
-		newGame: function () {
-
-			$("#start").hide();
-			$("#finish").hide();
-			$("#board").show();
-			$(".box").each(function () {
-				this.style.backgroundImage = "none";
-				$(this).removeClass("box-filled-1");
-				$(this).removeClass("box-filled-2");
-			});
-			$("#player2").removeClass("active");
-			$("#player1").addClass("active");
-		},
-
-		beginTurn: function () {
-
-			$(".box").each(function () {
-
-				$(this).unbind();
-
-				if ($(this).hasClass("box-filled-1") === false && $(this).hasClass("box-filled-2") === false) {
-
-					if ($("#player1").hasClass("active")) {
-
-						console.log("Set hover 1");
-
-						$(this).hover(
-
-							function () {
-								this.style.backgroundImage = "url('img/o.svg')";
-							}, function () {
-								this.style.backgroundImage = "none";
-							}
-						);
-					} else if ($("#player2").hasClass("active")) {
-
-						console.log("Set hover 2");
-
-						$(this).hover(
-
-							function () {
-								this.style.backgroundImage = "url('img/x.svg')";
-							}, function () {
-								this.style.backgroundImage = "none";
-							}
-						);
-					}
-				}
-			});
-
-			GameUI.play();
-		},
-
-		play: function () {
-
-			if ($("#player1").hasClass("active")) {
-
-				console.log("Play 1");
-
-				$(".box").each(function () {
-
-					if ($(this).hasClass("box-filled-1") === false && $(this).hasClass("box-filled-2") === false) {
-
-						$(this).click(function () {
-
-							$(this).addClass("box-filled-1");
-							$(this).unbind('mouseenter mouseleave');
-							this.style.backgroundImage = "url('img/o.svg')";
-							Game.checkForWin();
-							GameUI.nextTurn();
-						});
-					}
-				});
-
-			} else if ($("#player2").hasClass("active")) {
-
-				console.log("Play 2");
-
-				$(".box").each(function () {
-
-					if ($(this).hasClass("box-filled-1") === false && $(this).hasClass("box-filled-2") === false) {
-
-						$(this).click(function () {
-
-							$(this).addClass("box-filled-2");
-							$(this).unbind('mouseenter mouseleave');
-							this.style.backgroundImage = "url('img/x.svg')";
-							Game.checkForWin();
-							GameUI.nextTurn();
-						});
-					}
-				});	
-			}
-		},
-
-		nextTurn: function () {
-
-			if ($("#player1").hasClass("active")) {
-
-				console.log("Next turn 1");
-
-				$("#player1").removeClass("active");
-				$("#player2").addClass("active");
-
-				GameUI.beginTurn();
-
-			} else if ($("#player2").hasClass("active")) {
-
-				console.log("Next turn 2");
-
-				$("#player2").removeClass("active");
-				$("#player1").addClass("active");
-
-				GameUI.beginTurn();
-
-			} else {
-				console.log("No more turns!");
-			}
-		}
-	};
-
-}();
+}(gameModule || {}));
--- js/game_ui.js
+++ js/game_ui.js
@@ -1,121 +1,220 @@
-// var GameUI = {
-// 	newGame: function () {
+var gameModule = (function(Game){
 
-// 		$("#start").hide();
-// 		$("#finish").hide();
-// 		$("#board").show();
-// 		$(".box").each(function () {
-// 			this.style.backgroundImage = "none";
-// 			$(this).removeClass("box-filled-1");
-// 			$(this).removeClass("box-filled-2");
-// 		});
-// 		$("#player2").removeClass("active");
-// 		$("#player1").addClass("active");
-// 	},
+	var Game = { winner: "" };
 
-// 	beginTurn: function () {
+	Game.newGame = function () {
 
-// 		$(".box").each(function () {
+		$("#start").hide();
+		$("#finish").hide();
+		$("#board").show();
+		$(".box").each(function () {
+			this.style.backgroundImage = "none";
+			$(this).removeClass("box-filled-1");
+			$(this).removeClass("box-filled-2");
+		});
+		$("#player2").removeClass("active");
+		$("#player1").addClass("active");
+	}
 
-// 			$(this).unbind();
+	Game.beginTurn = function () {
 
-// 			if ($(this).hasClass("box-filled-1") === false && $(this).hasClass("box-filled-2") === false) {
+		$(".box").each(function () {
 
-// 				if ($("#player1").hasClass("active")) {
+			$(this).unbind();
 
-// 					console.log("Set hover 1");
+			if ($(this).hasClass("box-filled-1") === false && $(this).hasClass("box-filled-2") === false) {
 
-// 					$(this).hover(
+				if ($("#player1").hasClass("active")) {
 
-// 						function () {
-// 							this.style.backgroundImage = "url('img/o.svg')";
-// 						}, function () {
-// 							this.style.backgroundImage = "none";
-// 						}
-// 					);
-// 				} else if ($("#player2").hasClass("active")) {
+					console.log("Set hover 1");
 
-// 					console.log("Set hover 2");
+					$(this).hover(
 
-// 					$(this).hover(
+						function () {
+							this.style.backgroundImage = "url('img/o.svg')";
+						}, function () {
+							this.style.backgroundImage = "none";
+						}
+					);
+				} else if ($("#player2").hasClass("active")) {
 
-// 						function () {
-// 							this.style.backgroundImage = "url('img/x.svg')";
-// 						}, function () {
-// 							this.style.backgroundImage = "none";
-// 						}
-// 					);
-// 				}
-// 			}
-// 		});
+					console.log("Set hover 2");
 
-// 		GameUI.play();
-// 	},
+					$(this).hover(
 
-// 	play: function () {
+						function () {
+							this.style.backgroundImage = "url('img/x.svg')";
+						}, function () {
+							this.style.backgroundImage = "none";
+						}
+					);
+				}
+			}
+		});
 
-// 		if ($("#player1").hasClass("active")) {
+		Game.play();
+	}
 
-// 			console.log("Play 1");
+	Game.play = function () {
 
-// 			$(".box").each(function () {
+		if ($("#player1").hasClass("active")) {
 
-// 				if ($(this).hasClass("box-filled-1") === false && $(this).hasClass("box-filled-2") === false) {
+			console.log("Play 1");
 
-// 					$(this).click(function () {
+			$(".box").each(function () {
 
-// 						$(this).addClass("box-filled-1");
-// 						$(this).unbind('mouseenter mouseleave');
-// 						this.style.backgroundImage = "url('img/o.svg')";
-// 						Game.checkForWin();
-// 						GameUI.nextTurn();
-// 					});
-// 				}
-// 			});
+				if ($(this).hasClass("box-filled-1") === false && $(this).hasClass("box-filled-2") === false) {
 
-// 		} else if ($("#player2").hasClass("active")) {
+					$(this).click(function () {
 
-// 			console.log("Play 2");
+						$(this).addClass("box-filled-1");
+						$(this).unbind('mouseenter mouseleave');
+						this.style.backgroundImage = "url('img/o.svg')";
+						Game.checkForWin();
+						Game.nextTurn();
+					});
+				}
+			});
 
-// 			$(".box").each(function () {
+		} else if ($("#player2").hasClass("active")) {
 
-// 				if ($(this).hasClass("box-filled-1") === false && $(this).hasClass("box-filled-2") === false) {
+			console.log("Play 2");
 
-// 					$(this).click(function () {
+			$(".box").each(function () {
 
-// 						$(this).addClass("box-filled-2");
-// 						$(this).unbind('mouseenter mouseleave');
-// 						this.style.backgroundImage = "url('img/x.svg')";
-// 						Game.checkForWin();
-// 						GameUI.nextTurn();
-// 					});
-// 				}
-// 			});	
-// 		}
-// 	},
+				if ($(this).hasClass("box-filled-1") === false && $(this).hasClass("box-filled-2") === false) {
 
-// 	nextTurn: function () {
+					$(this).click(function () {
 
-// 		if ($("#player1").hasClass("active")) {
+						$(this).addClass("box-filled-2");
+						$(this).unbind('mouseenter mouseleave');
+						this.style.backgroundImage = "url('img/x.svg')";
+						Game.checkForWin();
+						Game.nextTurn();
+					});
+				}
+			});	
+		}
+	}
 
-// 			console.log("Next turn 1");
+	Game.nextTurn = function () {
 
-// 			$("#player1").removeClass("active");
-// 			$("#player2").addClass("active");
+		if ($("#player1").hasClass("active")) {
 
-// 			GameUI.beginTurn();
+			console.log("Next turn 1");
 
-// 		} else if ($("#player2").hasClass("active")) {
+			$("#player1").removeClass("active");
+			$("#player2").addClass("active");
 
-// 			console.log("Next turn 2");
+			Game.beginTurn();
 
-// 			$("#player2").removeClass("active");
-// 			$("#player1").addClass("active");
+		} else if ($("#player2").hasClass("active")) {
 
-// 			GameUI.beginTurn();
+			console.log("Next turn 2");
 
-// 		} else {
-// 			console.log("No more turns!");
-// 		}
-// 	}
-// };
+			$("#player2").removeClass("active");
+			$("#player1").addClass("active");
+
+			Game.beginTurn();
+
+		} else {
+			console.log("No more turns!");
+		}
+	}
+
+	Game.checkForWin = function () {
+		var boxes = [];
+		boxes = $(".box").toArray();
+		var check = [];
+
+		for (i = 0; i < boxes.length; i++) {
+			if (boxes[i].className.includes("box-filled-1")) {
+				check.push("player1");
+			} else if (boxes[i].className.includes("box-filled-2")) {
+				check.push("player2");
+			} else {
+				check.push("empty");
+			}
+		}
+		
+		if (check[0] !== "empty" && check[0] === check[1] && check[1] === check[2]) {
+
+			Game.winner = check[0];
+			Game.displayWinner();
+
+		} else if (check[3] !== "empty" && check[3] === check[4] && check[4] === check[5]) {
+
+			Game.winner = check[3];
+			Game.displayWinner();
+
+		} else if (check[6] !== "empty" && check[6] === check[7] && check[7] === check[8]) {
+			
+			Game.winner = check[6];
+			Game.displayWinner();
+
+		} else if (check[0] !== "empty" && check[0] === check[3] && check[3] === check[6]) {
+			
+			Game.winner = check[0];
+			Game.displayWinner();
+
+		} else if (check[1] !== "empty" && check[1] === check[4] && check[4] === check[7]) {
+					
+			Game.winner = check[1];
+			Game.displayWinner();
+
+		} else if (check[2] !== "empty" && check[2] === check[5] && check[5] === check[8]) {
+					
+			Game.winner = check[2];
+			Game.displayWinner();
+
+		} else if (check[0] !== "empty" && check[0] === check[4] && check[4] === check[8]) {
+					
+			Game.winner = check[0];
+			Game.displayWinner();
+
+		} else if (check[2] !== "empty" && check[2] === check[4] && check[4] === check[6]) {
+					
+			Game.winner = check[2];
+			Game.displayWinner();
+
+		} else if (check.includes("empty") === false){
+					
+			Game.winner = "Tie Game";
+			Game.displayWinner();
+		}
+	}
+
+	Game.displayWinner = function () {
+		if (Game.winner === "player1") {
+
+			$("#finish").removeClass("screen-win-two");
+			$("#finish").removeClass("screen-win-tie");
+			$(".message").html("Winner");
+			$("#finish").addClass("screen-win-one");
+			$("#finish").show();
+			$("#board").hide();
+
+		} else if (Game.winner === "player2") {
+
+			$("#finish").removeClass("screen-win-one");
+			$("#finish").removeClass("screen-win-tie");
+			$(".message").html("Winner");
+			$("#finish").addClass("screen-win-two");
+			$("#finish").show();
+			$("#board").hide();
+
+		} else if (Game.winner === "Tie Game") {
+
+			$("#finish").removeClass("screen-win-one");
+			$("#finish").removeClass("screen-win-two");
+			$(".message").html("It's a Tie!");
+			$("#finish").addClass("screen-win-tie");
+			$("#finish").show();
+			$("#board").hide();
+
+		}
+	}
+
+	return Game;
+
+}(gameModule || {}));
